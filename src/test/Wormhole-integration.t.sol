738baf9e1101029829bbb54f969ad1dfda4ba102
Sam MacPherson <xylom44@gmail.com>
2021-11-05 15:24:15 +0100

    add test scaffold

---
+++ src/test/Wormhole-integration.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.12;
+
+import "ds-test/test.sol";
+
+import "src/WormholeAuth.sol";
+import "src/WormholeFees.sol";
+import "src/WormholeGUID.sol";
+import "src/WormholeJoin.sol";
+
+interface Hevm {
+    function warp(uint256) external;
+    function roll(uint256) external;
+    function store(address,bytes32,bytes32) external;
+    function load(address,bytes32) external returns (bytes32);
+}
+
+contract WormholeIntegrationTest is DSTest {
+
+    Hevm hevm = Hevm(HEVM_ADDRESS);
+
+    function setUp() public {
+    }
+
+}
