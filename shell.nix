508cd14698487e182b7b0a15fc7b2ec989549c2f
olivdb <olivdb@protonmail.com>
2021-11-26 10:46:09 +0000

    Add GHA

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ url
+  , dappPkgs ? (
+    import (fetchTarball "https://github.com/makerdao/makerpkgs/tarball/master") {}
+  ).dappPkgsVersions.hevm-0_43_1
+}: with dappPkgs;
+
+mkShell {
+  DAPP_SOLC = solc-static-versions.solc_0_8_9 + "/bin/solc-0.8.9";
+  # No optimizations
+  SOLC_FLAGS = "";
+  buildInputs = [
+    dapp
+  ];
+
+  shellHook = ''
+    export NIX_SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+    unset SSL_CERT_FILE
+
+    export ETH_RPC_URL="''${ETH_RPC_URL:-${url}}"
+  '';
+}
