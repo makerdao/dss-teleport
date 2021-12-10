6b4b39e07713c191a24833135f51fb7afd0c8caa
Sam MacPherson <xylom44@gmail.com>
2021-12-10 09:48:24 -0500

    update guid precision (#11)

--- src/WormholeGUID.sol
+++ src/WormholeGUID.sol
@@ -23,8 +23,8 @@ struct WormholeGUID {
     address receiver;
     address operator;
     uint128 amount;
-    uint64 nonce;
-    uint64 timestamp;
+    uint80 nonce;
+    uint48 timestamp;
 }
 
 // solhint-disable-next-line func-visibility
