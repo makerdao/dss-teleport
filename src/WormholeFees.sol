41bfd40c4b1a327e72d4632273f536301f51d562
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-01-11 10:49:55 -0300

    Fix solhint warning

--- src/WormholeFees.sol
+++ src/WormholeFees.sol
@@ -20,5 +20,7 @@ import "./WormholeGUID.sol";
 
 // Calculate fees for a given Wormhole GUID
 interface WormholeFees {
-    function getFees(WormholeGUID calldata wormholeGUID, uint256 line, int256 debt, uint256 pending, uint256 amtToTake) external view returns (uint256 fees);
+    function getFees(
+        WormholeGUID calldata wormholeGUID, uint256 line, int256 debt, uint256 pending, uint256 amtToTake
+    ) external view returns (uint256 fees);
 }
