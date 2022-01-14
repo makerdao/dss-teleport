37eecc2801cf4ec7498ad2876ddd998273a06215
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-01-14 13:06:35 +0100

    Make ConstantFee respect TTL, add docs

--- src/WormholeConstantFee.sol
+++ src/WormholeConstantFee.sol
@@ -21,12 +21,28 @@ import {WormholeGUID} from "./WormholeGUID.sol";
 
 contract WormholeConstantFee is WormholeFees {
     uint256 immutable public fee;
+    uint256 immutable public ttl;
 
-    constructor(uint256 _fee) {
+    /**
+    * @param _fee - Constant fee in WAD
+    * @param _ttl - time in seconds to finalize flush (not wormhole). Usually, for optimistic rollups should be 8 days.
+    **/
+    constructor(uint256 _fee, uint256 _ttl) {
         fee = _fee;
+        ttl = _ttl;
     }
 
     function getFee(WormholeGUID calldata guid, uint256, int256, uint256, uint256 amtToTake) override external view returns (uint256) {
-        return guid.amount > 0 ? fee * amtToTake / guid.amount : 0;
+        // is slow withdrawal?
+        if (block.timestamp >= uint256(guid.timestamp) + ttl) {
+            return 0;
+        }
+
+        // no fees for empty wormholes
+        if (guid.amount == 0) {
+            return 0;
+        }
+
+        return fee * amtToTake / guid.amount;
     }
 }
