928efde0577dbf6fc68c8ac053df4b78461be546
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-01-17 10:30:02 +0100

    Drop - from natspec comments

--- src/WormholeConstantFee.sol
+++ src/WormholeConstantFee.sol
@@ -24,8 +24,8 @@ contract WormholeConstantFee is WormholeFees {
     uint256 immutable public ttl;
 
     /**
-    * @param _fee - Constant fee in WAD
-    * @param _ttl - time in seconds to finalize flush (not wormhole). Usually, for optimistic rollups should be 8 days.
+    * @param _fee Constant fee in WAD
+    * @param _ttl Time in seconds to finalize flush (not wormhole). Usually, for optimistic rollups should be 8 days.
     **/
     constructor(uint256 _fee, uint256 _ttl) {
         fee = _fee;
--- src/WormholeFees.sol
+++ src/WormholeFees.sol
@@ -23,11 +23,11 @@ interface WormholeFees {
     /**
     * @dev Return fee for particular wormhole. It should return 0 for wormholes that are being slow withdrawn. 
     * note: We define slow withdrawal as wormhole older than x. x has to be enough to finalize flush (not wormhole itself).
-    * @param wormholeGUID - Struct which contains the whole wormhole data
-    * @param line - debt ceiling
-    * @param debt - current debt
-    * @param pending - amount left to withdrawal
-    * @param amtToTake - amount to take. Can be less or equal to wormholeGUID.amount b/c of debt ceiling or because it is pending
+    * @param wormholeGUID Struct which contains the whole wormhole data
+    * @param line Debt ceiling
+    * @param debt Current debt
+    * @param pending Amount left to withdrawal
+    * @param amtToTake Amount to take. Can be less or equal to wormholeGUID.amount b/c of debt ceiling or because it is pending
     **/
     function getFee(
         WormholeGUID calldata wormholeGUID, uint256 line, int256 debt, uint256 pending, uint256 amtToTake
