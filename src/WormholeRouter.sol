9db72b5a4de7435e9140963f8c1ff902e557c997
Sam MacPherson <xylom44@gmail.com>
2022-02-02 11:42:02 -0800

    Readded the operator fee (#61)
    * readded the operator fee
    
    * gas snapshot
    
    * fix line length
    
    * add fail case for operator fee too high
    
    * gas-snapshot

--- src/WormholeRouter.sol
+++ src/WormholeRouter.sol
@@ -24,7 +24,11 @@ interface TokenLike {
 }
 
 interface GatewayLike {
-    function requestMint(WormholeGUID calldata wormholeGUID, uint256 maxFee) external returns (uint256 postFeeAmount);
+    function requestMint(
+        WormholeGUID calldata wormholeGUID,
+        uint256 maxFeePercentage,
+        uint256 operatorFee
+    ) external returns (uint256 postFeeAmount);
     function settle(bytes32 sourceDomain, uint256 batchedDaiToFlush) external;
 }
 
@@ -119,13 +123,14 @@ contract WormholeRouter {
      * @notice Call a GatewayLike contract to request the minting of DAI. The sender must be a supported gateway
      * @param wormholeGUID The wormhole GUID to register
      * @param maxFeePercentage Max percentage of the withdrawn amount (in WAD) to be paid as fee (e.g 1% = 0.01 * WAD)
+     * @param operatorFee The amount of DAI to pay to the operator
      * @return postFeeAmount The amount of DAI sent to the receiver after taking out fees
      */
-    function requestMint(WormholeGUID calldata wormholeGUID, uint256 maxFeePercentage) external returns (uint256 postFeeAmount) {
+    function requestMint(WormholeGUID calldata wormholeGUID, uint256 maxFeePercentage, uint256 operatorFee) external returns (uint256 postFeeAmount) {
         require(msg.sender == gateways[wormholeGUID.sourceDomain], "WormholeRouter/sender-not-gateway");
         address gateway = gateways[wormholeGUID.targetDomain];
         require(gateway != address(0), "WormholeRouter/unsupported-target-domain");
-        return GatewayLike(gateway).requestMint(wormholeGUID, maxFeePercentage);
+        return GatewayLike(gateway).requestMint(wormholeGUID, maxFeePercentage, operatorFee);
     }
 
     /**
