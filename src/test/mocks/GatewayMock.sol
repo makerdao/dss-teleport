8022905ee407f38b8b234bef3137b3869d44133a
olivdb <olivdb@protonmail.com>
2022-02-04 12:47:16 +0000

    Fix warning

--- src/test/mocks/GatewayMock.sol
+++ src/test/mocks/GatewayMock.sol
@@ -4,6 +4,6 @@ pragma solidity 0.8.9;
 import "src/WormholeGUID.sol";
 
 contract GatewayMock {
-    function requestMint(WormholeGUID calldata, uint256, uint256) external returns (uint256 postFeeAmount) {}
+    function requestMint(WormholeGUID calldata, uint256, uint256) external pure returns (uint256 postFeeAmount) {}
     function settle(bytes32 sourceDomain, uint256 batchedDaiToFlush) external {}
 }
