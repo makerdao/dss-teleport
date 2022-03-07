a64f8de344d465c7e58d550241db63e8e1137d58
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-03-07 17:09:56 -0300

    Use solc 0.8.11 and optimizations on

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+export DAPP_BUILD_OPTIMIZE=1
+export DAPP_BUILD_OPTIMIZE_RUNS=200
+
 if [[ -z "$1" && -z "$2" ]]; then
-    dapp --use solc:0.8.9 test --fuzz-runs 1 -vv
+    dapp --use solc:0.8.11 test --fuzz-runs 1 -vv
 elif [[ -z "$2" ]]; then
-    dapp --use solc:0.8.9 test --match "$1" --fuzz-runs 1 -vv
+    dapp --use solc:0.8.11 test --match "$1" --fuzz-runs 1 -vv
 elif [[ -z "$1" ]]; then
-    dapp --use solc:0.8.9 test --fuzz-runs "$2" -vv
+    dapp --use solc:0.8.11 test --fuzz-runs "$2" -vv
 else
-    dapp --use solc:0.8.9 test --match "$1" --fuzz-runs "$2" -vv
+    dapp --use solc:0.8.11 test --match "$1" --fuzz-runs "$2" -vv
 fi
