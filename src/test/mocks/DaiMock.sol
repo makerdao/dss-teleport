0b83dfbd0f868aa8dec70b84c5c0271fa95714bf
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-06-07 09:36:41 -0300

    Use solc 0.8.14

--- src/test/mocks/DaiMock.sol
+++ src/test/mocks/DaiMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.8.13;
+pragma solidity 0.8.14;
 
 contract DaiMock {
     mapping (address => uint)                      public balanceOf;
--- test.sh
+++ test.sh
@@ -5,11 +5,11 @@ export DAPP_BUILD_OPTIMIZE=1
 export DAPP_BUILD_OPTIMIZE_RUNS=200
 
 if [[ -z "$1" && -z "$2" ]]; then
-    dapp --use solc:0.8.13 test --fuzz-runs 1 -vv
+    dapp --use solc:0.8.14 test --fuzz-runs 1 -vv
 elif [[ -z "$2" ]]; then
-    dapp --use solc:0.8.13 test --match "$1" --fuzz-runs 1 -vv
+    dapp --use solc:0.8.14 test --match "$1" --fuzz-runs 1 -vv
 elif [[ -z "$1" ]]; then
-    dapp --use solc:0.8.13 test --fuzz-runs "$2" -vv
+    dapp --use solc:0.8.14 test --fuzz-runs "$2" -vv
 else
-    dapp --use solc:0.8.13 test --match "$1" --fuzz-runs "$2" -vv
+    dapp --use solc:0.8.14 test --match "$1" --fuzz-runs "$2" -vv
 fi
