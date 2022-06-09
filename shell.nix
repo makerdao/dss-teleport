3cfc2e8e7ed9b1f4c53addcc036001309ad3ff83
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-06-09 09:09:48 -0300

    Use solc 0.8.14 (#85)

--- shell.nix
+++ shell.nix
@@ -1,11 +1,10 @@
 { dappPkgs ? (
     import (fetchTarball "https://github.com/makerdao/makerpkgs/tarball/master") {}
-  ).dappPkgsVersions.master-20220325
+  ).dappPkgsVersions.master-20220524
 }: with dappPkgs;
 
 mkShell {
-  DAPP_SOLC = solc-static-versions.solc_0_8_13 + "/bin/solc-0.8.13";
-  SOLC_FLAGS = "--optimize --optimize-runs=200";
+  DAPP_SOLC = solc-static-versions.solc_0_8_14 + "/bin/solc-0.8.14";
   buildInputs = [
     dapp
   ];
