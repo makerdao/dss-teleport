3cbd5570d76716793c7c61ce3054aedce5176e52
Sam MacPherson <xylom44@gmail.com>
2022-10-25 06:03:35 -0400

    Update src/test/TeleportJoin.t.sol
    Co-authored-by: Gonzalo Balabasquer <gbalabasquer@gmail.com>

--- src/test/TeleportJoin.t.sol
+++ src/test/TeleportJoin.t.sol
@@ -517,8 +517,7 @@ contract TeleportJoinTest is DSTest {
         assertEq(join.debt("l2network"), 0);
 
         vat.suck(address(0), address(this), 100_000 * RAD);
-        daiJoin.exit(address(this), 100_000 ether);
-        dai.transfer(address(join), 100_000 ether);
+        daiJoin.exit(address(join), 100_000 ether);
 
         join.settle("l2network", domain, 100_000 ether);
 
