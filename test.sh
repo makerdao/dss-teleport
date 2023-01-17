5f880a81dbaba1150a5501417c43cf505ffe7fda
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2023-01-17 14:40:22 -0300

    Migrate to forge (#102)
    * Migrate to forge
    
    * dapp uninstall ds-test
    
    * dapp install foundry-rs/forge-std
    
    * Use forge-std and replace hevm by vm
    
    * Fix config.yaml
    
    * Replace try functions with forge way to evaluate reverts + removing cache directory from repository
    
    * Add env variable
    
    * Fix imports
    
    * Remove shell.nix
    
    * Use older version of prover until some issues are fixed
    
    * Update src/test/TeleportOracleAuth.t.sol
    
    Co-authored-by: olivdb <olivdb@protonmail.com>
    
    * Add certain form of coverage back
    
    Co-authored-by: olivdb <olivdb@protonmail.com>

--- test.sh
+++ test.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-[[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+[[ "$ETH_RPC_URL" && "$(cast chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-export DAPP_BUILD_OPTIMIZE=1
-export DAPP_BUILD_OPTIMIZE_RUNS=200
+export FOUNDRY_OPTIMIZER=true
+export FOUNDRY_OPTIMIZER_RUNS=200
 
-if [[ -z "$1" && -z "$2" ]]; then
-    dapp --use solc:0.8.15 test --rpc-url="$ETH_RPC_URL" --fuzz-runs 1 -vv
-elif [[ -z "$2" ]]; then
-    dapp --use solc:0.8.15 test --rpc-url="$ETH_RPC_URL" --match "$1" --fuzz-runs 1 -vv
-elif [[ -z "$1" ]]; then
-    dapp --use solc:0.8.15 test --rpc-url="$ETH_RPC_URL" --fuzz-runs "$2" -vv
+if [[ -z "$1" ]]; then
+    forge test --use solc:0.8.15 --fork-url "$ETH_RPC_URL" -vv
 else
-    dapp --use solc:0.8.15 test --rpc-url="$ETH_RPC_URL" --match "$1" --fuzz-runs "$2" -vv
+    forge test --use solc:0.8.15 --fork-url "$ETH_RPC_URL" --match "$1" -vv
 fi
