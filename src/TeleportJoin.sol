6cb76d60a1a59b49e26903b4822a8e3e26b33746
olivdb <olivdb@protonmail.com>
2023-03-22 21:22:03 +0000

    Update src/TeleportJoin.sol
    Co-authored-by: Gonzalo Balabasquer <gbalabasquer@gmail.com>

--- src/TeleportJoin.sol
+++ src/TeleportJoin.sol
@@ -23,7 +23,6 @@ interface VatLike {
     function live() external view returns (uint256);
     function urns(bytes32, address) external view returns (uint256, uint256);
     function frob(bytes32, address, address, address, int256, int256) external;
-    function grab(bytes32, address, address, address, int256, int256) external;
     function hope(address) external;
     function move(address, address, uint256) external;
     function nope(address) external;
